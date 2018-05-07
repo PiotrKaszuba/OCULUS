91cf36b19122c907813159f295e1e8d10789413e
PiotrKaszuba <piotr.kaszuba@student.put.poznan.pl>
2018-05-07 07:16:31 +0200

    poprawka

--- Code/Algorithms/Models.py
+++ Code/Algorithms/Models.py
@@ -51,8 +51,8 @@ class Models:
         if numb > 0:
             self.model.load_weights(self.path + str(numb))
 
-    def metrics(pred,true):
-        return met.customMetric(pred,true)
+    def metr(pred, true):
+        return met.customMetric(pred, true)
 
     def validate(self, pathForce=None):
 
@@ -81,7 +81,7 @@ class Models:
             pred = pred.reshape((self.rowDim, self.colDim))
             #print(met.centerDiff(pred,true,check=False))
             #print(met.binaryDiff(pred,true))
-            print("Custom metric: " +str(self.metrics(pred, true)))
+            print("Custom metric: " +str(met.customMetric(pred, true)))
             x = []
 
             x.append(pic[0][0].reshape((self.rowDim, self.colDim,self.channels)))
--- Code/Train.py
+++ Code/Train.py
@@ -5,7 +5,7 @@ from Code.Libraries import DataAugmentationClasses as dac
 
 #params
 base_path='../'
-image_size_level=15
+image_size_level=20
 base_scale=0.75
 
 batch_size = 32
@@ -37,14 +37,14 @@ class_mode='mask'
 show_function = mol.model_show_function
 read_function = mol.read_and_size
 validate_path_provider_func = mol.random_path
-validate_start_path = base_path+'Images/awaiting/'
+validate_start_path = base_path+'Images/valildate/'
 
-filters=2
+filters=12
 
 load_weights=True
 weights_path="../weights/unet"
 var_filename="../weights/var.txt"
-validate=False
+validate=True
 check_perf_times=3
 check_perf_times_in_loop=0
 learn_rate = 1e-04
